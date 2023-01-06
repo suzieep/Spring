de42888e5ff197c794ab269dc5a3bc47e11f732c
suzieep <wnsuwls0707@gmail.com>
2023-01-06 18:19:25 +0900

    change dir structure

---
+++ prototype/src/test/java/videocall/prototype/PrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package videocall.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
